0d73e96a7243fb8ef589f6d629f17654cb9d9e05
Malik <malikhussein229@gmail.com>
2024-05-27 18:06:29 +0300

    Fixing pycodestyle error

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,18 +12,19 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close_storage(exception):
     """
-    
+
     """
     storage.close()
-    
+
 
 @app.errorhandler(404)
 def not_found(error):
     """
-    
+
     """
     response = {"error": "Not found"}
     return jsonify(response), 404
