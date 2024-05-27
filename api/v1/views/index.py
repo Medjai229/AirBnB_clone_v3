2449a2bf89c02c986d8a0f540683f0046efaeb92
Malik <malikhussein229@gmail.com>
2024-05-27 17:50:28 +0300

    Task#4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ index
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -13,3 +14,19 @@ def api_status():
     """
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route('/stats')
+def api_stats():
+    """
+    
+    """
+    stats = {
+        "amenities": storage.count("Amenity"), 
+        "cities": storage.count("City"), 
+    	"places": storage.count("Place"), 
+    	"reviews": storage.count("Review"), 
+    	"states": storage.count("State"), 
+        "users": storage.count("User")
+        }
+    
+    return jsonify(stats)
