82da023645a883542b71f912557784ae3b69ca55
Malik <malikhussein229@gmail.com>
2024-05-27 18:02:19 +0300

    Task#5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 app
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,6 +11,22 @@ from api.v1.views import app_views
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """
+    
+    """
+    storage.close()
+    
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
 
 
 if __name__ == "__main__":
